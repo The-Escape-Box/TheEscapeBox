3f1f26ffea220dd16e47e4dee1e750936f7b06e9
Mohmad Naser Alnakeshbandi <76824323+Mohmad-Naser-alnakeshbandi@users.noreply.github.com>
2024-02-26 22:16:08 +0100

    Add chasing enemy

--- UnityProject/Assets/DemonTree/Assets/Script/DemonMovement.cs
+++ UnityProject/Assets/DemonTree/Assets/Script/DemonMovement.cs
@@ -1,54 +1,78 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.AI;
 
-public class DemonMovement : MonoBehaviour {
-	private Animator anim;
-	int hIdles;
-	int hAngry;
-	int hAttack;
-	int hGrabs;
-	int hThumbsUp;
-		
-	// Use this for initialization
-	void Start () {
-		anim = GetComponent<Animator> ();
-		hIdles = Animator.StringToHash("Idles");
-		hAngry = Animator.StringToHash("Angry");
-		hAttack = Animator.StringToHash("Attack");
-		hGrabs = Animator.StringToHash("Grabs");
-		hThumbsUp = Animator.StringToHash("ThumbsUp");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	        if (Input.GetKeyDown(KeyCode.W)) {
-			if(anim.GetCurrentAnimatorStateInfo(0).IsName("Idles")) {
-				anim.SetBool(hIdles, false);
-				anim.SetBool(hAngry, true);
-	             }
-	        } else if (Input.GetKeyDown(KeyCode.S)) {
-			if(anim.GetCurrentAnimatorStateInfo(0).IsName("Idles")) {
-				anim.SetBool(hIdles, false);
-				anim.SetBool(hAttack, true);
-			    }
-	        } else if (Input.GetKeyDown(KeyCode.A)) {
-			if(anim.GetCurrentAnimatorStateInfo(0).IsName("Idles")) {
-				anim.SetBool(hIdles, false);
-				anim.SetBool(hGrabs, true);
-			    }
-			} else if (Input.GetKeyDown(KeyCode.D)) {
-			if(anim.GetCurrentAnimatorStateInfo(0).IsName("Idles")) {
-				anim.SetBool(hIdles, false);
-				anim.SetBool(hThumbsUp, true);
-			    }
-		    } else {
-			if(!anim.GetCurrentAnimatorStateInfo(0).IsName("Idles")) {
-				anim.SetBool(hIdles, true);
-				anim.SetBool(hAngry, false);
-				anim.SetBool(hAttack, false);
-				anim.SetBool(hGrabs, false);
-				anim.SetBool(hThumbsUp, false);
-			}
-		}
-	}
+public class DemonMovement : MonoBehaviour
+{
+    public Transform player; // Public variable to assign the player in the editor
+    private NavMeshAgent agent;
+    private Animator anim;
+
+    int hIdles;
+    int hAngry;
+    int hAttack;
+    int hGrabs;
+
+    public float attackDistance = 5f; // Distance at which the demon attacks
+    public float grabDistance = 2f;   // Distance at which the demon grabs the player
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
+        // Set up hash IDs for animation states
+        hIdles = Animator.StringToHash("Idles");
+        hAngry = Animator.StringToHash("Angry");
+        hAttack = Animator.StringToHash("Attack");
+        hGrabs = Animator.StringToHash("Grabs");
+    }
+
+    void Update()
+    {
+        // Set the destination of the NavMeshAgent to the player's position
+        agent.destination = player.position;
+
+        // Check distance to player
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        // Determine which animation to play based on distance
+        if (distanceToPlayer <= attackDistance)
+        {
+            // Player is close enough to attack
+            Attack();
+        }
+        else if (distanceToPlayer <= grabDistance)
+        {
+            // Player is too close, use Grabs animation
+            Grabs();
+        }
+        else
+        {
+            // Player is far, return to idle state
+            UpdateAnimation(false);
+        }
+    }
+
+    void UpdateAnimation(bool isMoving)
+    {
+        // Set animation parameters based on movement
+        anim.SetBool(hIdles, !isMoving);
+        anim.SetBool(hAngry, false); // Assuming Angry animation is not movement-based
+        anim.SetBool(hAttack, false); // Assuming Attack animation is not movement-based
+        anim.SetBool(hGrabs, false); // Assuming Grabs animation is not movement-based
+    }
+
+    void Attack()
+    {
+        // Trigger attack animation
+        UpdateAnimation(false); // Stop any movement animation
+        anim.SetBool(hAttack, true);
+    }
+
+    void Grabs()
+    {
+        // Trigger grabs animation
+        UpdateAnimation(false); // Stop any movement animation
+        anim.SetBool(hGrabs, true);
+    }
 }
