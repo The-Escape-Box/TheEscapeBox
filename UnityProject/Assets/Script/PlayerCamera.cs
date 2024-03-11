b877c0ea87cfdd0b32f0f1079c62295fb6844bb6
Mohmad Naser Alnakeshbandi <76824323+Mohmad-Naser-alnakeshbandi@users.noreply.github.com>
2024-03-11 23:35:42 +0100

    add lukas requirment ;)

--- UnityProject/Assets/Script/PlayerCamera.cs
+++ UnityProject/Assets/Script/PlayerCamera.cs
@@ -29,7 +29,7 @@ namespace Script
             float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
             xRotation -= mouseY;
-            xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Clamp the rotation here
+            xRotation = Mathf.Clamp(xRotation, -60f, 60f); // Clamp the rotation here
 
             transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
             playerBody.Rotate(Vector3.up * mouseX);
