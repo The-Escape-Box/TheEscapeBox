1fcde6f80a5e4819f530a4c00373bce47e0ba6a4
Mohmad Naser Alnakeshbandi <76824323+Mohmad-Naser-alnakeshbandi@users.noreply.github.com>
2024-04-18 14:09:52 +0200

    add sounds
    cinema sound
    Environment sound
    Sleep room sound

---
+++ UnityProject/Assets/Script/sounds/EnviromentSound.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnviromentSound : MonoBehaviour
+{
+    public float triggerDistance = 5.0f; // Distance at which the sound will play
+    public AudioSource audioSource; // Reference to the AudioSource component
+
+    void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player"); // Find the cinema object
+        if (player != null)
+        {
+            float distance = Vector3.Distance(transform.position, player.transform.position); // Calculate the distance
+            print(distance);
+            if (distance <= triggerDistance && !audioSource.isPlaying)
+            {
+                audioSource.Play(); // Play the sound if too close and not already playing
+            }
+            else if (distance > triggerDistance && audioSource.isPlaying)
+            {
+                audioSource.Stop(); // Stop the sound if the player is far enough away
+            }
+        }
+    }
+}
